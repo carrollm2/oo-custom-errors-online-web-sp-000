a64bcee33d7351c7f6fc8a20cdc94624e7ce8144
Michael Carroll <carrollm2@gmail.com>
2019-11-12 12:59:01 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -21,4 +21,7 @@ beyonce.get_married("Jay-Z")
 puts beyonce.name
 
 class PartnerError < StandardError
+  def message
+    "you must give the get_married method an argument of an instance of the person class!"
+  end
 end
